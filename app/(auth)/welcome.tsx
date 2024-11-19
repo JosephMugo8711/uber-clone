3392dcf3269ea14771afe6ccbbadde4f70988c21
JosephMugo8711 <wamiti8711@gmail.com>
2024-11-19 13:01:33 +0300

    Folders and files

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome Abroad Broad your time to shine is near</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,27 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const styles = StyleSheet.create({
+  link: {
+    // Add your styles here
+    color: "blue",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+});
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View>
+        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+          This screen doesn't exist.
+        </Text>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <Text>Go to home screen!</Text>
         </Link>
-      </ThemedView>
+      </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
